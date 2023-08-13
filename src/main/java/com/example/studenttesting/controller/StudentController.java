160b204b0a7926319f149feb9d8c843d81b0677f
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-08-13 11:35:15 +0200

    Test Apis

--- src/main/java/com/example/studenttesting/controller/StudentController.java
+++ src/main/java/com/example/studenttesting/controller/StudentController.java
@@ -7,11 +7,7 @@ import com.example.studenttesting.model.StudentDto;
 import com.example.studenttesting.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,24 +17,24 @@ public class StudentController {
     private StudentService studentService;
 
     @PostMapping("/create")
-    public ResponseEntity<StudentDto> createStudent(Student student){
+    public ResponseEntity<StudentDto> createStudent(@RequestBody Student student){
         return ResponseEntity.ok(studentService.createStudent(student));
     }
     @GetMapping("/getAll")
-    public ResponseEntity<List<StudentDto>> getAllStudent(Student student){
+    public ResponseEntity<List<StudentDto>> getAllStudent(){
         return ResponseEntity.ok(studentService.getAllStudent());
     }
-    @DeleteMapping("/delete")
-    public ResponseEntity deleteStudent(Long id){
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteStudent(@PathVariable Long id){
         studentService.deleteStudent(id);
         return ResponseEntity.ok().build();
     }
-    @PostMapping("/edit")
-    public ResponseEntity<StudentDto> editStudent(Student student){
+    @PutMapping("/edit")
+    public ResponseEntity<StudentDto> editStudent(@RequestBody Student student){
         return ResponseEntity.ok(studentService.editStudent(student));
     }
-    @GetMapping("/get")
-    public ResponseEntity<StudentDto> getStudentById(Long id){
+    @GetMapping("/get/{id}")
+    public ResponseEntity<StudentDto> getStudentById(@PathVariable Long id){
         return ResponseEntity.ok(studentService.getStudentById(id));
     }
 }
